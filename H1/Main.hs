562817f1fe93b2f0c0cf945233a06e12b2ea8596
afy <hannes.furhoff@hotmail.com>
2024-05-17 08:18:52 +0200

    H1 done

--- H1/Main.hs
+++ H1/Main.hs
@@ -6,6 +6,11 @@ import Data.List
 import Data.Tuple
 
 
+-- Override for 3-tuple snd
+lastElement :: (a,b,c) -> c
+lastElement (_,_,c) = c 
+
+
 -- Calculate size of subset (sum of elements)
 calcSum :: [Int] -> Int
 calcSum [] = 0
@@ -14,29 +19,45 @@ calcSum (x:xs)
  | otherwise = x + calcSum xs
 
 
+-- Get list of all subsets
+getSubsets :: [Int] -> [ (Int,Int,[Int]) ]
+getSubsets v = [ getslice v i j | i <- [0..length v - 1], j <- [i..length v - 1 ] ] 
+ where getslice v i j = (i,j, [v !! vi| vi <- [i..j] ] )
+
+
 -- Qsort to insert the subset tuple according to sum (Tuple[0])
-insertSubset :: (Int,[Int]) -> [ (Int,[Int]) ] -> [ (Int,[Int]) ]
-insertSubset (a,b) v = [xl | xl <- v, fst xl <= a] ++ [(a,b)] ++ [xu | xu <- v, fst xu > a]
+insertSubset :: (Int,Int,[Int]) -> [ (Int,Int,[Int]) ] -> [ (Int,Int,[Int]) ]
+insertSubset (i,j,x) v = [xl | xl <- v, calcSum (lastElement xl) <= calcSum x] 
+                      ++ [(i,j,x)] 
+                      ++ [xu | xu <- v, calcSum (lastElement xu) > calcSum x]
 
 
 -- Iterate through original set and transform to ordered list according to calcsize
-transformSubsets :: [ [Int] ] -> [ (Int,[Int]) ]
-transformSubsets (x:xs) 
+orderSubsets :: [ (Int,Int,[Int]) ] -> [ (Int,Int,[Int]) ]
+orderSubsets ((i,j,v):xs) 
  | null xs = [element]
- | otherwise = insertSubset element (transformSubsets xs)
- where element = (calcSum x, x)
-
-
--- Get list of all subsets
-getSubsets :: [Int] -> [ [Int] ]
-getSubsets v = [ getslice v i j | i <- [0..length v - 1], j <- [i..length v - 1 ] ] 
- where getslice v i j = [ v !! vi | vi <- [i..j] ]
-
-getSet :: [Int] -> [ (Int,[Int]) ]
-getSet = transformSubsets . getSubsets
+ | otherwise = insertSubset element (orderSubsets xs)
+ where element = (i,j,v)
 
 
 -- Return k min sizes
-getKset :: [Int] -> Int -> [ (Int,[Int]) ]
-getKset v k = [ ps !! vi | vi <- [0..k-1] ]
- where ps = getSet v
+getKset :: [Int] -> Int -> [ (Int,Int,[Int]) ]
+getKset v k = [ ps !! (k-vi-1) | vi <- [0..k-1] ]
+ where ps = (orderSubsets . getSubsets) v
+
+
+-- Final step; convert to printout format
+getKstr :: [ (Int,Int,[Int]) ] -> String
+getKstr ((i,j,x):xs) 
+ | null(xs) = "Size\ti\tj\tList\n" ++ lineform i j x
+ | otherwise = getKstr xs ++ lineform i j x
+ where lineform i j x = show(calcSum x)++"\t"++show(i+1)++"\t"++show(j+1)++"\t"++show(x)++"\n"
+
+
+smallestKset xs k = putStr (getKstr (getKset xs k))
+
+
+-- let atest = [x*(-1)^x | x <- [1..100]]
+-- smallestKset atest 15
+-- smallestKset [24,-11,-34,42,-24,7,-19,21] 6
+-- smallestKset  [3,2,-4,3,2,-5,-2,2,3,-3,2,-5,6,-2,2,3] 8
